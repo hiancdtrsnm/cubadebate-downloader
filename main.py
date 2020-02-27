2c2aa1543d4b3b11609f5dbd48467f4533cf69e4
hian <hiancdtrsnm@gmail.com>
2020-02-27 18:59:12 -0500

    Add folder for date

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def main(from_date: datetime, to_date: datetime, folder: Path = typer.Option(
     resolve_path=True,
 )):
 
-    os.makedirs(folder, exist_ok=True)
 
     folder = str(folder)
     manager = enlighten.get_manager()
     ticks = manager.counter(desc='News', unit='news')
 
     for new in get_news(from_date.date(), to_date.date()):
-        path = os.path.join(folder, new['title']+'.json')
+        os.makedirs(os.path.join(folder, str(new['date'])), exist_ok=True)
+        path = os.path.join(folder, str(new['date']), new['title']+'.json')
         json.dump(new, open(path, 'w'), indent=2,
                   default=str, ensure_ascii=False)
         print(f"{new['title']}-{new['date']}")
